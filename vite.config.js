15a185d9b68ea4643a6e58742b9cb2cbac8a51cd
Kshitiz Kumar Maurya <117600399+kshitiz79@users.noreply.github.com>
2025-01-04 02:22:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: '/', // Use '/' for root-level deployment on Netlify
   plugins: [react()],
-})
+});
